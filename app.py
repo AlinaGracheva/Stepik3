117fd7033e8d4cf70ad94af71e4766ae4d7c5847
AlinaGracheva <a1inagracheva@mail.ru>
2020-12-02 17:51:24 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session, redirect
 from models import db, migrate, Category, Meal
 from config import Config
-
+import forms
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
@@ -12,9 +12,13 @@ def main_view():
     if type(session["cart"]) is int or session.get("cart") is None:
         session["cart"] = []
     cart = session["cart"]
-    meals_sum = 0
-    for price in db.session.query(Meal.price).filter(Meal.id.in_(cart)).all():
-        meals_sum += price[0]
+    meals_in_cart = []
+    for item in set(cart):
+        meal = db.session.query(Meal).get(item)
+        meal_item = {'id': meal.id, 'title': meal.title, 'price': meal.price, 'description': meal.description,
+                     'picture': meal.picture, 'quantity': cart.count(item)}
+        meal_item['sum'] = meal_item['price'] * meal_item['quantity']
+        meals_in_cart.append(meal_item)
     user = session.get("user")
     if user is None:
         is_login = False
@@ -24,7 +28,7 @@ def main_view():
         for meal in db.session.query(Meal).filter_by(category_id=category.id).order_by(db.func.random()).limit(3).all():
             meals[category.title].append(
                 {'id': meal.id, 'title': meal.title, 'price': meal.price, 'description': meal.description, 'picture': meal.picture})
-    return render_template("main.html", meals=meals, cart=cart, sum=meals_sum, is_login=is_login)
+    return render_template("main.html", meals=meals, cart=cart, meals_in_cart=meals_in_cart, is_login=is_login)
 
 
 @app.route('/addtocart/<int:meal_id>/')
@@ -32,26 +36,27 @@ def addtocart_view(meal_id):
     if type(session["cart"]) is int or session.get("cart") is None:
         session["cart"] = []
     cart = session["cart"]
-    print(cart)
     cart.append(meal_id)
     session['cart'] = cart
-    print(cart)
-    meals_sum = 0
-    for price in db.session.query(Meal.price).filter(Meal.id.in_(cart)).all():
-        meals_sum += price[0]
     return redirect('/cart/')
 
 
 @app.route('/cart/')
 def cart_view():
+    form = forms.OrderForm()
     cart = session["cart"]
-    meals_sum = 0
-    for price in db.session.query(Meal.price).filter(Meal.id.in_(cart)).all():
-        meals_sum += price[0]
+    meals_in_cart = []
+    for item in set(cart):
+        meal = db.session.query(Meal).get(item)
+        meal_item = {'id': meal.id, 'title': meal.title, 'price': meal.price, 'description': meal.description,
+                     'picture': meal.picture, 'quantity': cart.count(item)}
+        meal_item['sum'] = meal_item['price'] * meal_item['quantity']
+        meals_in_cart.append(meal_item)
     user = session.get("user")
     if user is None:
         is_login = False
-    return render_template("cart.html", cart=cart, sum=meals_sum, is_login=is_login)
+
+    return render_template("cart.html", cart=cart, meals_in_cart=meals_in_cart, is_login=is_login, form=form)
 
 
 @app.route('/account/')
@@ -59,7 +64,7 @@ def account_view():
     return render_template("account.html")
 
 
-@app.route('/login/')
+@app.route('/auth/')
 def login_view():
     return render_template("login.html")
 
@@ -80,4 +85,4 @@ def order_view():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,4 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, HiddenField, RadioField
 from wtforms.fields.html5 import TelField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Email
+
+
+class OrderForm(FlaskForm):
+    name = StringField("Ваше имя", [InputRequired(message="Поле должно быть заполнено")])
+    address = StringField("Адрес", [InputRequired(message="Поле должно быть заполнено")])
+    mail = StringField("Электронная почта", [InputRequired(message="Поле должно быть заполнено"), Email(message="Некорректный адрес электронной почты")], render_kw={'autofocus': True})
+    phone = TelField("Телефон", [InputRequired(message="Поле должно быть заполнено")])
+    order_summ  = HiddenField()
+    order_cart = HiddenField()
+    submit = SubmitField("Оформить заказ")
